b29acaedf21e70da6b1d41c6e0548d81ebe6bc74
twedzony <t.wedzony@gmail.com>
2017-03-12 14:41:27 +0100

    1.1

--- jquery-examples/animate/main.js
+++ jquery-examples/animate/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
-	$( "p" ).add( "div" ).css("background-color", "yellow");
+	$( ".start" ).click(function(){
+		$( ".animated" )
+		.animate({ height: "toggle" })
+	});
+
+	$( ".left" ).click(function(){
+		$( ".animated" )
+		.animate({ marginRight: "+=40px" })
+	});
+
+	$( ".right" ).click(function(){
+		$( ".animated" )
+		.animate({ marginLeft: "+=40px" })
+	});
 
 });
+
+
