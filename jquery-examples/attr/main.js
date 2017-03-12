040056585833f7bb4493b261133ca90d1e32d47a
twedzony <t.wedzony@gmail.com>
2017-03-12 21:34:58 +0100

    1.1

--- jquery-examples/attr/main.js
+++ jquery-examples/attr/main.js
@@ -1,5 +1,22 @@
 $(function(){
 
-	$( "p" ).add( "div" ).css("background-color", "yellow");
+	alert( $( "img" ).attr( "alt" ) );
 
 });
+
+/*
+
+it's also possible to set new attributes
+
+.attr( "src", "new source");
+.attr( "alt", "new alt");
+
+or
+
+.attr({
+	src : "google.pl/foo.png",
+	alt : "some new alt",
+	title : "some new title" 
+});
+
+*/
