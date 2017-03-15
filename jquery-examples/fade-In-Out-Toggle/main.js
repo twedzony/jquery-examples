8becb569df1a0fb4e39f0a267b94360a716f63e5
twedzony <t.wedzony@gmail.com>
2017-03-15 23:18:40 +0100

    1.1

--- jquery-examples/fade-In-Out-Toggle/main.js
+++ jquery-examples/fade-In-Out-Toggle/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
-	$( "p" ).add( "div" ).css("background-color", "yellow");
+	$( ".btn-fadein" ).click(function(){
+		$( ".foo, .btn-fadeout " ).fadeIn(1000);
+		$( " .btn-fadein " ).fadeOut(0);
+	});
+
+	$( ".btn-fadeout" ).click(function(){
+		$( ".foo" ).fadeOut(1000);
+		$( ".btn-fadein " ).fadeIn(0);
+		$( ".btn-fadeout" ).fadeOut(0);
+	});
+
+	$( ".btn-toggle" ).click(function(){
+		$( ".foo" ).toggle(1000);
+	});
+
 
 });
+
