1b2189317de0336ca8b711d62ec5546bdc6cc13e
twedzony <t.wedzony@gmail.com>
2017-05-10 21:07:05 +0200

    1.1

--- jquery-examples/get/main.js
+++ jquery-examples/get/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
-	$( "p" ).add( "div" ).css("background-color", "yellow");
+	var foo = $( "li ").get( -2 );
+
+	$( foo ).clone().appendTo( ".test" );
 
 });
+
+/*	
+
+	'get' is useless if you don't use negative values
+ 	in this case it's enough to use array like: 
+
+ 	var foo = $( "li ")[2];
+
+ 	to get 3rd element for example
+
+ */
