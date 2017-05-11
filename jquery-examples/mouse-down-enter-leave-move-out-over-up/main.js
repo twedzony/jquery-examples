63dd4b57594141097db0e11db0defc6b3c0ddced
twedzony <t.wedzony@gmail.com>
2017-05-11 23:01:51 +0200

    1.1

--- jquery-examples/mouse-down-enter-leave-move-out-over-up/main.js
+++ jquery-examples/mouse-down-enter-leave-move-out-over-up/main.js
@@ -1,5 +1,25 @@
 $(function(){
 
-	$( "p" ).add( "div" ).css("background-color", "yellow");
+	$( ".outer" ).mouseenter(function(){
+		$( ".test" ).append("mouseenter<br/>");
+	})
+
+	$( ".outer" ).mouseleave(function(){
+		$( ".test" ).append("mouseleave<br/>");
+	})
+	$( ".outer" ).mouseover(function(){
+		$( ".test" ).append("mouseover<br/>");
+	})
+
+	$( ".outer" ).mouseout(function(){
+		$( ".test" ).append("mouseout<br/>");
+	})
+	$( ".outer" ).mousedown(function(){
+		$( ".test" ).append("mousedown<br/>");
+	})
+
+	$( ".outer" ).mouseup(function(){
+		$( ".test" ).append("mouseup<br/>");
+	})
 
 });
