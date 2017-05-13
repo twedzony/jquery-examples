e4ec3ca9286d1221603fbccf3a36fbfe3fe2ccd9
twedzony <t.wedzony@gmail.com>
2017-05-13 12:45:03 +0200

    1.1

--- jquery-examples/on-one-off/main.js
+++ jquery-examples/on-one-off/main.js
@@ -1,5 +1,15 @@
 $(function(){
 
-	$( "p" ).add( "div" ).css("background-color", "yellow");
+	$(document).one("click", "div", function(){
+		$(".test").append("click ");
+	})
 
+	$(document).on("click", "span", function(){
+		$(".test").append("click ");
+	})
+
+	$(document).one("click", "p", function(){
+		$(document).off("click", "span");
+		$(".test").append("disabled ");
+	})
 });
